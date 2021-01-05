a8193f48ef5314bfbe84253cfeb39ededc5da3c2
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-05 17:14:06 -0600

    updated reducer function to include update_categories switch statement case

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -6,11 +6,19 @@ import {
 
 export const reducer = (state, action) => {
     switch (action.type) {
+        
         // if action type value is the value of 'UPDATE_PRODUCTS', return a new state object with an updated products array
         case UPDATE_PRODUCTS:
             return {
                 ...state,
-                products: [...action.products],
+                products: [...action.products]
+            };
+
+        // if action type value is the value of 'UPDATE_CATEGORIES', return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
             };
 
         // if it's none of these actions, do not update state at all and keep things THE SAME!
