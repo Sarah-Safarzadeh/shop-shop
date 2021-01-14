b979fd9a1a3387f693b2d5f32f52a52689846721
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-14 14:11:10 -0600

    display cart closed icon

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -11,6 +11,18 @@ const Cart = () => {
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
+
+  // display the closed shopping cart icon
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
