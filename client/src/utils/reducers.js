f62f8b93b48fef16108196d65fb293deec12ac9f
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-14 13:12:15 -0600

    cart import statements

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,7 +4,13 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from "./actions";
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
+} from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
