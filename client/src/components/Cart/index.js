89664d403d0204a0618a3bfe1372cbcc6d45e818
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-14 14:03:31 -0600

    global store and the toggle action imports

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+
 
 const Cart = () => {
     return (
